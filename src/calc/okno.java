7b3a640d0c0ee2477cff4968e9a52daf83092bc9
fujimen <marlenadomalewska805@gmail.com>
2020-11-21 19:51:20 +0100

    Added GUI

---
+++ src/calc/okno.java
@@ -0,0 +1,21 @@
+package calc;
+
+import javax.swing.*;
+
+public class okno {
+    private JPanel Okno;
+    private JTextField a54564TextField;
+    private JTextField textField2;
+    private JLabel label1;
+    private JLabel label2;
+    private JButton buttonPlus;
+    private JButton buttonMinus;
+    private JButton buttonMult;
+    private JButton buttonDiv;
+    private JLabel LabelWynik;
+    private JTextField textFieldWynik;
+    private JButton buttonPrzejmij;
+    private JButton ButtonWyczysc;
+
+
+}
